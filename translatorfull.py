a980f2b396798abda9c23ba16a0b75c7f4bb9945
Ashutosh-Chandra-2003 <156914801+Ashutosh-Chandra-2003@users.noreply.github.com>
2026-02-16 11:23:16 +0530

    Update translatorfull.py

--- translatorfull.py
+++ translatorfull.py
@@ -310,7 +310,7 @@ class ModernTranslatorUI:
             fg="white",
             relief="flat",
             command=self.simplify
-        ).pack(pady=(0, 25))
+        ).pack(pady=(0, 10))
 
     def show_waiting(self):
         self.hindi_label.config(text="")
